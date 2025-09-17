LawnchairLauncher/lawnchair

Adds support for shrinking nav handle on press
Fix: 314158312
Flag: LEGACY SHRINK_NAV_HANDLE_ON_PRESS DISABLED
Test: Manual

Change-Id: Icd46467939ae9a38592ee67ee0d2a521eb8e0b6c

src/com/android/launcher3/config/FeatureFlags.java
@@ -124,6 +124,10 @@ public final class FeatureFlags {
             getReleaseFlag(308693847, "ANIMATE_LPNH", TEAMFOOD,
                     "Animates navbar when long pressing");
 
+    public static final BooleanFlag SHRINK_NAV_HANDLE_ON_PRESS =
+            getReleaseFlag(314158312, "SHRINK_NAV_HANDLE_ON_PRESS", DISABLED,
+                    "Shrinks navbar when long pressing if ANIMATE_LPNH is enabled");
+
     public static final IntFlag LPNH_SLOP_PERCENTAGE =
             getIntFlag(301680992, "LPNH_SLOP_PERCENTAGE", 100,
                     "Controls touch slop percentage for lpnh");

PixelBuildsROM/android_packages_apps_Launcher3

src/com/android/launcher3/config/FeatureFlags.java
@@ -131,6 +131,10 @@ public final class FeatureFlags {
             getReleaseFlag(308693847, "ANIMATE_LPNH", ENABLED,
                     "Animates navbar when long pressing");
 
+    public static final BooleanFlag SHRINK_NAV_HANDLE_ON_PRESS =
+            getReleaseFlag(314158312, "SHRINK_NAV_HANDLE_ON_PRESS", ENABLED,
+                    "Shrinks navbar when long pressing if ANIMATE_LPNH is enabled");
+
     public static final BooleanFlag ENABLE_SHOW_KEYBOARD_OPTION_IN_ALL_APPS = getReleaseFlag(
             270394468, "ENABLE_SHOW_KEYBOARD_OPTION_IN_ALL_APPS", ENABLED,
             "Enable option to show keyboard when going to all-apps");