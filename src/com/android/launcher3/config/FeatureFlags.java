LawnchairLauncher/lawnchair

Add animateNavBarLongPress to SystemUiProxy and ANIMATE_LPNH flag
Bug: 306400785
Bug: 308693847
Test: Manual
Flag: LEGACY ANIMATE_LPNH TEAMFOOD
Change-Id: I996c923e5a1949e6219f900da12e2b5091597750

src/com/android/launcher3/config/FeatureFlags.java
@@ -119,6 +119,10 @@ public final class FeatureFlags {
             getReleaseFlag(301680992, "CUSTOM_LPNH_THRESHOLDS", DISABLED,
                     "Add dev options to customize the LPNH trigger slop and milliseconds");
 
+    public static final BooleanFlag ANIMATE_LPNH =
+            getReleaseFlag(308693847, "ANIMATE_LPNH", TEAMFOOD,
+                    "Animates navbar when long pressing");
+
     public static final IntFlag LPNH_SLOP_PERCENTAGE =
             getIntFlag(301680992, "LPNH_SLOP_PERCENTAGE", 100,
                     "Controls touch slop percentage for lpnh");

PixelBuildsROM/android_packages_apps_Launcher3

src/com/android/launcher3/config/FeatureFlags.java
@@ -127,6 +127,10 @@ public final class FeatureFlags {
             getDebugFlag(301680992, "CUSTOM_LPNH_THRESHOLDS", DISABLED,
                     "Add dev options to customize the LPNH trigger slop and milliseconds");
 
+    public static final BooleanFlag ANIMATE_LPNH =
+            getReleaseFlag(308693847, "ANIMATE_LPNH", ENABLED,
+                    "Animates navbar when long pressing");
+
     public static final BooleanFlag ENABLE_SHOW_KEYBOARD_OPTION_IN_ALL_APPS = getReleaseFlag(
             270394468, "ENABLE_SHOW_KEYBOARD_OPTION_IN_ALL_APPS", ENABLED,
             "Enable option to show keyboard when going to all-apps");