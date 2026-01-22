ad849075e9a8eff4bd84a399be8cd328b5ad7ee3
Adnan Begovic <abegovic@google.com>
2026-01-21 16:01:09 -0800

    Introduce LauncherProcessImageListener for downscaling images in Launcher.
    This listener implements ImageDecoder.OnHeaderDecodedListener to automatically downscale images during decoding if their width or height exceeds a specified maximum memory constraint. This helps prevent excessive memory usage when loading large images in the Launcher process.
    
    Bug: 468061774
    Flag: com.android.graphics.hwui.flags.image_decoder_default_listener
    Test: LauncherProcessImageListenerTest.kt
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:960fc6f7799ca578630140d683e67dba66ed9c29
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:88640014b648993d0ce15fb82aebf2e19cec94b4
    Merged-In: I79d705160a12efd0f4235a4c6bfd104b5b873dfa
    Change-Id: I79d705160a12efd0f4235a4c6bfd104b5b873dfa

--- quickstep/src/com/android/quickstep/QuickstepProcessInitializer.java
+++ quickstep/src/com/android/quickstep/QuickstepProcessInitializer.java
@@ -23,11 +23,15 @@ import android.os.Looper;
 import android.os.Trace;
 import android.os.UserManager;
 import android.util.Log;
+import android.graphics.ImageDecoder;
 import android.view.ThreadedRenderer;
 
 import com.android.launcher3.BuildConfig;
 import com.android.launcher3.MainProcessInitializer;
+import com.android.launcher3.R;
 import com.android.systemui.shared.system.InteractionJankMonitorWrapper;
+
+import java.util.Set;
 
 @SuppressWarnings("unused")
 @TargetApi(Build.VERSION_CODES.R)
@@ -63,6 +67,8 @@ public class QuickstepProcessInitializer extends MainProcessInitializer {
         ThreadedRenderer.setContextPriority(
                 ThreadedRenderer.EGL_CONTEXT_PRIORITY_HIGH_IMG);
 
+        setupImageDecoder(context);
+
         // Enable Looper trace points.
         // This allows us to see Handler callbacks on traces.
         Looper.getMainLooper().setTraceTag(Trace.TRACE_TAG_APP);
@@ -72,4 +78,16 @@ public class QuickstepProcessInitializer extends MainProcessInitializer {
                     call.descriptor + " called on mainthread under " + call.activeTrace));
         }
     }
+
+    private void setupImageDecoder(Context context) {
+        // Limit the max memory usage.
+        int maxMemoryMb = context.getResources().getInteger(R.integer.max_launcher_memory_mb);
+        long maxMemoryBytes = maxMemoryMb * 1024L * 1024L;
+        // Get the allowed mime types from the resources.
+        Set<String> allowedMimeTypes = Set.of(context.getResources().getStringArray(
+                R.array.allowed_image_mime_types));
+        ImageDecoder.setDefaultProcessListener(
+                new LauncherProcessImageListener(maxMemoryBytes, allowedMimeTypes));
+    }
+
 }
